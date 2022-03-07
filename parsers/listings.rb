efb6663c6aa290c7a97d2ef05b2daf66d4e216d5
fyofio <muhammadisrofiordi@gmail.com>
2022-03-08 04:55:29 +0800

    added all award year in seed, listing capture all books

---
+++ parsers/listings.rb
@@ -0,0 +1,12 @@
+nokogiri = Nokogiri.HTML(content)
+
+# Cari link semua buku di halaman
+books = nokogiri.css('div.category.clearFix')
+books.each do |book|
+    url = "https://www.goodreads.com" + book.at_css('a')["href"]
+    pages << {
+          url: url,
+          page_type: 'books',
+          fetch_type: "browser"
+    }
+end
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -5,3 +5,83 @@ pages << {
   url: "https://www.goodreads.com/choiceawards/best-books-2011",
   fetch_type: "browser"
 }
+
+pages << {
+  page_type: "listings",
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  url: "https://www.goodreads.com/choiceawards/best-books-2012",
+  fetch_type: "browser"
+}
+
+pages << {
+  page_type: "listings",
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  url: "https://www.goodreads.com/choiceawards/best-books-2013",
+  fetch_type: "browser"
+}
+
+pages << {
+  page_type: "listings",
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  url: "https://www.goodreads.com/choiceawards/best-books-2014",
+  fetch_type: "browser"
+}
+
+pages << {
+  page_type: "listings",
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  url: "https://www.goodreads.com/choiceawards/best-books-2015",
+  fetch_type: "browser"
+}
+
+pages << {
+  page_type: "listings",
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  url: "https://www.goodreads.com/choiceawards/best-books-2016",
+  fetch_type: "browser"
+}
+
+pages << {
+  page_type: "listings",
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  url: "https://www.goodreads.com/choiceawards/best-books-2017",
+  fetch_type: "browser"
+}
+
+pages << {
+  page_type: "listings",
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  url: "https://www.goodreads.com/choiceawards/best-books-2018",
+  fetch_type: "browser"
+}
+
+pages << {
+  page_type: "listings",
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  url: "https://www.goodreads.com/choiceawards/best-books-2019",
+  fetch_type: "browser"
+}
+
+pages << {
+  page_type: "listings",
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  url: "https://www.goodreads.com/choiceawards/best-books-2020",
+  fetch_type: "browser"
+}
+
+pages << {
+  page_type: "listings",
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  url: "https://www.goodreads.com/choiceawards/best-books-2021",
+  fetch_type: "browser"
+}
