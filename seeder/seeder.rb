b93c85fe027ded7ff5152c683473be8be161d4f6
fyofio <muhammadisrofiordi@gmail.com>
2022-03-08 04:27:48 +0800

    added config and seeder file

---
+++ seeder/seeder.rb
@@ -0,0 +1,7 @@
+pages << {
+  page_type: "listings",
+  method: "GET",
+  headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
+  url: "https://www.goodreads.com/choiceawards/best-books-2011",
+  fetch_type: "browser"
+}
