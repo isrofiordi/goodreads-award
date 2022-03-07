f2e8372653c4ba4c76e898bea8b7544e40525c08
fyofio <muhammadisrofiordi@gmail.com>
2022-03-08 06:00:28 +0800

    added book parser

--- parsers/books.rb
+++ parsers/books.rb
@@ -1,7 +1,48 @@
 nokogiri = Nokogiri.HTML(content)
 
 # initialize an empty hash
-product = {}
+book = {}
 
 #save the url
-product['url'] = page['vars']['url']
+book['url'] = page['vars']['url']
+
+# image url
+book['img_url'] = nokogiri.at_css('span.cover a img')["src"]
+
+# title
+book['Title'] = nokogiri.at_css('a.winningTitle').text.strip unless nokogiri.at_css('a.winningTitle').nil?
+
+# author
+book['Author'] = nokogiri.at_css('span[itemprop="name"]').text.strip unless nokogiri.at_css('span[itemprop="name"]').nil?
+
+# award year
+book['Award Year'] = book['url'][/\d+/,0] unless book['url'].nil?
+
+# award category
+book['Award Category'] = nokogiri.at_css('div.gcaMastheader').text.strip unless nokogiri.at_css('div.gcaMastheader').nil?
+
+# number of votes
+book["Number of Votes"] = nokogiri.at_css('span.gcaNumVotes').text.strip.gsub("\n"," ") + " out of " + nokogiri.at_css('div.gcaNumVotes').text.strip.gsub("\n"," ")
+
+# All nominees
+book['Nominees'] = []
+
+nominee_node = nokogiri.css("div.inlineblock.pollAnswer.resultShown")
+
+nominee_node.each do |nom|
+    book['Nominees'] << nom.at_css("img")['alt']
+end
+
+# description
+if nokogiri.at_css('div#description span[style="display:none"]')
+    book['Description'] = nokogiri.at_css('div#description span[style="display:none"]').strip
+else
+    book['Description'] = nokogiri.at_css('div#description').text.strip
+end
+
+
+# specify the collection where this record will be stored
+book['_collection'] = 'books'
+
+# save the product to the job’s outputs
+outputs << book
