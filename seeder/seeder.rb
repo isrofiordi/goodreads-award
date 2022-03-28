5adc580b8f864fc9db746d6e96fd9c2c331d569a
fyofio <muhammadisrofiordi@gmail.com>
2022-03-29 05:56:10 +0800

    revert back

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -3,9 +3,6 @@ pages << {
   method: "GET",
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
   url: "https://www.goodreads.com/choiceawards/best-books-2011",
-  vars:{
-    nama:'coba1'
-  },
   fetch_type: "browser"
 }
 
@@ -13,10 +10,7 @@ pages << {
   page_type: "listings",
   method: "GET",
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
-  url: "https://www.goodreads.com/choiceawards/best-books-2011",
-  vars:{
-    nama:'coba2'
-  },
+  url: "https://www.goodreads.com/choiceawards/best-books-2012",
   fetch_type: "browser"
 }
 
